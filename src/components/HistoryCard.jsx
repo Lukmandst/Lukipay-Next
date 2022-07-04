8ef85bfa45a5765c602dc3f7a352ca54991e3f45
Lukmandst <lukmandwi11@gmail.com>
2022-07-04 11:28:37 +0700

    add conditional

--- src/components/HistoryCard.jsx
+++ src/components/HistoryCard.jsx
@@ -2,14 +2,16 @@ import style from "components/styles/HistoryCard.module.css";
 import Image from "next/image";
 import { currencyFormatter } from "helpers/formatter";
 
-function HistoryCard({ history , phone, user }) {
+function HistoryCard({ history, phone, user }) {
   return (
     <>
       <div className={style.historyCard}>
         <div className={style.infoWrapperHistory}>
           <div className={style.imgHistory}>
             <Image
-              src={`${process.env.NEXT_PUBLIC_IMG}${user? user.image : history && history.image}`}
+              src={`${process.env.NEXT_PUBLIC_IMG}${
+                user ? user.image : history && history.image
+              }`}
               alt="img"
               layout="fixed"
               width={52}
@@ -18,28 +20,40 @@ function HistoryCard({ history , phone, user }) {
             />
           </div>
           <div className={style.nametypeWrapper}>
-            <div className={style.nameHistory}>{user? user.firstName +' '+ user.lastName: history && history.fullName}</div>
-            <div className={style.typeHistory}>{user? user.noTelp: history &&history.type }</div>
+            <div className={style.nameHistory}>
+              {user
+                ? user.firstName + " " + user.lastName
+                : history && history.fullName}
+            </div>
+            <div className={style.typeHistory}>
+              {user ? user.noTelp : history && history.type}
+            </div>
           </div>
         </div>
-        {
-        user? <></>: history &&
-        history.type === "send" && history.status === "success" ? (
+        {user ? (
+          <></>
+        ) : history &&
+          history.type === "send" &&
+          history.status === "success" ? (
           <div className={style.amountHistoryRed}>
-           - {currencyFormatter.format(history.amount)}
+            - {currencyFormatter.format(history.amount)}
           </div>
         ) : history && history.status === "pending" ? (
           <div className={style.amountHistoryGray}>
             {currencyFormatter.format(history.amount)}
           </div>
-        ) : history && history.type === "topup" && history.status === "success" ? (
+        ) : (history && history.type === "topup") ||
+          history.type === "accept" ||
+          history.status === "success" ? (
           <div className={style.amountHistoryGreen}>
             + {currencyFormatter.format(history.amount)}
           </div>
-        ) : history && (
-          <div className={style.amountHistoryGray}>
-            {currencyFormatter.format(history.amount)}
-          </div>
+        ) : (
+          history && (
+            <div className={style.amountHistoryGray}>
+              {currencyFormatter.format(history.amount)}
+            </div>
+          )
         )}
       </div>
     </>
